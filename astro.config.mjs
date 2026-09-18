c66a2123e4390e8966e57c68615aa8a82571230b
speakgoodchinese <speakgoodchinese@gmail.com>
2026-09-18 12:02:25 +0800

    electron build workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { unified } from '@astrojs/markdown-remark';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   base: './', // required by Electron
-  markdown: {
-    processor: unified(),
-  },
   site: 'https://singbaobao.com',
   integrations: [react()]
 });
